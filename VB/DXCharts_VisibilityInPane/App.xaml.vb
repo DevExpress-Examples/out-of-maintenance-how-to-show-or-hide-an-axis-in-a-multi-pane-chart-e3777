7bbf17e5f42e1c09ac8d21adb96200c03ad68a2c
DevExpressExampleBot <fakeEmail>
2022-12-18 20:44:58 +0400

    Source auto update [skip ci]

---
+++ VB/DXCharts_VisibilityInPane/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace DXCharts_VisibilityInPane
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
